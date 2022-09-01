c9a47958197f84c7a3212717a1f585d73ef48afa
Kiwg <gkiwure21@gmail.com>
2022-09-01 17:01:17 +0300

    Added Js content

---
+++ index.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+
+function save(){
+    var Name = document.getElementById("Name").value;
+    var Address = document.getElementById("Address").value;
+    var Contact= document.getElementById("Contact").value;
+
+
+
+
+    if(Name === '' || Address === '' || Contact === '' || Symptom1 === '' || Symptom2 === '' || Symptom3 === ''){
+      alert("You haven't entered details");
+    }
+
+    else{
+        Swal.fire(Name + ' We have received your details. Thank you for choosing us.');
+
+    }
+
+       //document.patient-details.reset();
+
+     
+     event.preventDefault();
+}
+  
+     
+function upload(){
+    var Symptom1 = document.getElementById("Symptom1").value;
+    var Symptom2 = document.getElementById("Symptom2").value;
+    var Symptom3 = document.getElementById("Symptom3").value;
+
+    if(Symptom1 === '' || Symptom2 === '' || Symptom3 === ''){
+        alert("You haven't entered any details");
+      }
+
+    else{
+        Swal.fire(' We have received your details. Thank you for choosing us.');
+
+    }
+
+    event.preventDefault();
+}
+
+
+ 
+ 
